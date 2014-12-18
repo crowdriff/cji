c28f13f3ae3a35d14840668e802aff3fd1b534df
Peter Kieltyka <peter.kieltyka@pressly.com>
2014-12-18 17:34:32 -0500

    Support for standard middlewares too

--- cji.go
+++ cji.go
@@ -7,21 +7,26 @@ import (
 	"github.com/zenazn/goji/web"
 )
 
-// Goji's middleware type signature
-type MiddlewareFunc func(*web.C, http.Handler) http.Handler
-
 type cji struct {
-	middlewares []MiddlewareFunc
+	middlewares []interface{}
 }
 
-func Use(middlewares ...MiddlewareFunc) *cji {
-	return &cji{middlewares}
+func Use(middlewares ...interface{}) *cji {
+	return (&cji{}).Use(middlewares...)
 }
 
-func (z *cji) Use(middlewares ...MiddlewareFunc) *cji {
-	mw := z.middlewares
-	mw = append(mw, middlewares...)
-	return &cji{mw}
+func (z *cji) Use(middlewares ...interface{}) *cji {
+	c := &cji{z.middlewares}
+	for _, mw := range middlewares {
+		switch t := mw.(type) {
+		default:
+			panic(fmt.Sprintf("unsupported middleware type: %T", t))
+		case func(http.Handler) http.Handler:
+		case func(*web.C, http.Handler) http.Handler:
+		}
+		c.middlewares = append(c.middlewares, mw)
+	}
+	return c
 }
 
 // Compose together the middleware chain and wrap the handler with it
@@ -50,18 +55,23 @@ func (z *cji) On(handler interface{}) web.HandlerFunc {
 
 // Wrap takes a middleware that works on http.Handler and returns a function that
 // takes a web.HandlerFunc and returns a web.HandlerFunc. We use this to wrap HandlerFuncs
-func wrap(middleware MiddlewareFunc) func(web.HandlerFunc) web.HandlerFunc {
+func wrap(middleware interface{}) func(web.HandlerFunc) web.HandlerFunc {
 	fn := func(hf web.HandlerFunc) web.HandlerFunc {
 		return func(c web.C, w http.ResponseWriter, r *http.Request) {
 			newFn := func(ww http.ResponseWriter, rr *http.Request) {
 				hf(c, ww, rr)
 			}
-			fn, ok := middleware(&c, http.HandlerFunc(newFn)).(http.HandlerFunc)
-			if ok {
-				fn(w, r)
-			} else {
-				panic("unsupported handler passed to the chain!")
+
+			var fn http.HandlerFunc
+			switch mw := middleware.(type) {
+			default:
+				panic(fmt.Sprintf("unsupported middleware type: %T", mw))
+			case func(http.Handler) http.Handler:
+				fn = mw(http.HandlerFunc(newFn)).(http.HandlerFunc)
+			case func(*web.C, http.Handler) http.Handler:
+				fn = mw(&c, http.HandlerFunc(newFn)).(http.HandlerFunc)
 			}
+			fn(w, r)
 		}
 	}
 	return fn
